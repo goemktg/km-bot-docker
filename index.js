7fd65d7dd31a9e91589f3cd3fa128eac88ace533
goemktg <samktg52@gmail.com>
2023-08-13 06:16:15 +0900

    feat: 여러 기능 추가 feat - 시작 시 디버그 모드인지 로그 기록 feat - 더 나은 embed 내용물

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ client.login(process.env.DISCORD_TOKEN);
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
+	console.log('debug: '+process.env.DEBUG);
 
 	client.user.setActivity('뉴비', { type: ActivityType.Watching });
 
@@ -44,8 +45,12 @@ async function processKillmail(debug = false) {
 		var killboardResponse = await fetch("https://redisq.zkillboard.com/listen.php", { method: "GET"} );
 		var redisqData = await JSON.parse(await killboardResponse.text());
 
+		// overload data if debug
 		if (debug) {
-			killboardResponse = '{"package":{"killID":110946162,"killmail":{"attackers":[{"damage_done":239865,"faction_id":500021,"final_blow":false,"security_status":0,"ship_type_id":30212},{"character_id":91266835,"corporation_id":98438347,"damage_done":24964,"final_blow":true,"security_status":-0.3,"ship_type_id":29988,"weapon_type_id":29988},{"character_id":90803088,"corporation_id":98578021,"damage_done":8633,"final_blow":false,"security_status":-1.6,"ship_type_id":29986,"weapon_type_id":29986},{"character_id":322718659,"corporation_id":98578021,"damage_done":6798,"final_blow":false,"security_status":5,"ship_type_id":29986,"weapon_type_id":27401},{"character_id":90273223,"corporation_id":98438347,"damage_done":5864,"final_blow":false,"security_status":-0.8,"ship_type_id":29988,"weapon_type_id":2446},{"character_id":2113436920,"corporation_id":98739488,"damage_done":966,"faction_id":500003,"final_blow":false,"security_status":4.7,"ship_type_id":47466,"weapon_type_id":47466},{"character_id":450024655,"corporation_id":98490492,"damage_done":832,"final_blow":false,"security_status":5,"ship_type_id":29988,"weapon_type_id":2466}],"killmail_id":110946162,"killmail_time":"2023-08-12T12:57:09Z","solar_system_id":31001074,"victim":{"alliance_id":1220922756,"character_id":2120222883,"corporation_id":98691843,"damage_taken":287922,"items":[{"flag":14,"item_type_id":8135,"quantity_destroyed":1,"singleton":0},{"flag":5,"item_type_id":30013,"quantity_destroyed":8,"singleton":0},{"flag":20,"item_type_id":41054,"quantity_destroyed":1,"singleton":0},{"flag":25,"item_type_id":16535,"quantity_dropped":1,"singleton":0},{"flag":5,"item_type_id":33474,"quantity_destroyed":1,"singleton":0},{"flag":94,"item_type_id":26086,"quantity_destroyed":1,"singleton":0},{"flag":30,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":33,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":32,"item_type_id":33450,"quantity_dropped":1,"singleton":0},{"flag":16,"item_type_id":8135,"quantity_destroyed":1,"singleton":0},{"flag":27,"item_type_id":16475,"quantity_dropped":1,"singleton":0},{"flag":5,"item_type_id":4613,"quantity_dropped":1,"singleton":0},{"flag":28,"item_type_id":33450,"quantity_dropped":1,"singleton":0},{"flag":5,"item_type_id":33475,"quantity_dropped":1,"singleton":0},{"flag":29,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":28,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":11,"item_type_id":1248,"quantity_destroyed":1,"singleton":0},{"flag":29,"item_type_id":33450,"quantity_destroyed":1,"singleton":0},{"flag":5,"item_type_id":76114,"quantity_destroyed":6,"singleton":0},{"flag":32,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":33,"item_type_id":33450,"quantity_dropped":1,"singleton":0},{"flag":92,"item_type_id":26374,"quantity_destroyed":1,"singleton":0},{"flag":24,"item_type_id":2281,"quantity_destroyed":1,"singleton":0},{"flag":5,"item_type_id":24507,"quantity_destroyed":15366,"singleton":0},{"flag":30,"item_type_id":33450,"quantity_dropped":1,"singleton":0},{"flag":31,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":12,"item_type_id":1248,"quantity_destroyed":1,"singleton":0},{"flag":15,"item_type_id":8135,"quantity_destroyed":1,"singleton":0},{"flag":5,"item_type_id":28668,"quantity_dropped":17,"singleton":0},{"flag":87,"item_type_id":31890,"quantity_destroyed":2,"singleton":0},{"flag":87,"item_type_id":31890,"quantity_dropped":3,"singleton":0},{"flag":22,"item_type_id":2281,"quantity_dropped":1,"singleton":0},{"flag":87,"item_type_id":31894,"quantity_destroyed":3,"singleton":0},{"flag":87,"item_type_id":31894,"quantity_dropped":2,"singleton":0},{"flag":93,"item_type_id":4397,"quantity_destroyed":1,"singleton":0},{"flag":17,"item_type_id":2048,"quantity_dropped":1,"singleton":0},{"flag":21,"item_type_id":13953,"quantity_destroyed":1,"singleton":0},{"flag":19,"item_type_id":5443,"quantity_dropped":1,"singleton":0},{"flag":5,"item_type_id":17938,"quantity_dropped":1,"singleton":0},{"flag":31,"item_type_id":33450,"quantity_dropped":1,"singleton":0},{"flag":87,"item_type_id":2488,"quantity_destroyed":4,"singleton":0},{"flag":87,"item_type_id":2488,"quantity_dropped":2,"singleton":0},{"flag":23,"item_type_id":41218,"quantity_destroyed":1,"singleton":0},{"flag":13,"item_type_id":1248,"quantity_destroyed":1,"singleton":0}],"position":{"x":-106675567338.35883,"y":-53284704281.52799,"z":637580135899.0309},"ship_type_id":47466}},"zkb":{"locationID":40406890,"hash":"df5710fe59fd96e09cbaa381cd52906bc7f919a6","fittedValue":374428881.32,"droppedValue":43312011.33,"destroyedValue":378840295.1,"totalValue":422152306.43,"points":7,"npc":false,"solo":false,"awox":false,"labels":["cat:6","#:5+","pvp","loc:w-space"],"href":"https://esi.evetech.net/v1/killmails/110946162/df5710fe59fd96e09cbaa381cd52906bc7f919a6/"}}}'
+			// case kill
+			// killboardResponse = '{"package":{"killID":110946162,"killmail":{"attackers":[{"damage_done":239865,"faction_id":500021,"final_blow":false,"security_status":0,"ship_type_id":30212},{"character_id":91266835,"corporation_id":98438347,"damage_done":24964,"final_blow":true,"security_status":-0.3,"ship_type_id":29988,"weapon_type_id":29988},{"character_id":90803088,"corporation_id":98578021,"damage_done":8633,"final_blow":false,"security_status":-1.6,"ship_type_id":29986,"weapon_type_id":29986},{"character_id":322718659,"corporation_id":98578021,"damage_done":6798,"final_blow":false,"security_status":5,"ship_type_id":29986,"weapon_type_id":27401},{"character_id":90273223,"corporation_id":98438347,"damage_done":5864,"final_blow":false,"security_status":-0.8,"ship_type_id":29988,"weapon_type_id":2446},{"character_id":2113436920,"corporation_id":98739488,"damage_done":966,"faction_id":500003,"final_blow":false,"security_status":4.7,"ship_type_id":47466,"weapon_type_id":47466},{"character_id":450024655,"corporation_id":98490492,"damage_done":832,"final_blow":false,"security_status":5,"ship_type_id":29988,"weapon_type_id":2466}],"killmail_id":110946162,"killmail_time":"2023-08-12T12:57:09Z","solar_system_id":31001074,"victim":{"alliance_id":1220922756,"character_id":2120222883,"corporation_id":98691843,"damage_taken":287922,"items":[{"flag":14,"item_type_id":8135,"quantity_destroyed":1,"singleton":0},{"flag":5,"item_type_id":30013,"quantity_destroyed":8,"singleton":0},{"flag":20,"item_type_id":41054,"quantity_destroyed":1,"singleton":0},{"flag":25,"item_type_id":16535,"quantity_dropped":1,"singleton":0},{"flag":5,"item_type_id":33474,"quantity_destroyed":1,"singleton":0},{"flag":94,"item_type_id":26086,"quantity_destroyed":1,"singleton":0},{"flag":30,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":33,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":32,"item_type_id":33450,"quantity_dropped":1,"singleton":0},{"flag":16,"item_type_id":8135,"quantity_destroyed":1,"singleton":0},{"flag":27,"item_type_id":16475,"quantity_dropped":1,"singleton":0},{"flag":5,"item_type_id":4613,"quantity_dropped":1,"singleton":0},{"flag":28,"item_type_id":33450,"quantity_dropped":1,"singleton":0},{"flag":5,"item_type_id":33475,"quantity_dropped":1,"singleton":0},{"flag":29,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":28,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":11,"item_type_id":1248,"quantity_destroyed":1,"singleton":0},{"flag":29,"item_type_id":33450,"quantity_destroyed":1,"singleton":0},{"flag":5,"item_type_id":76114,"quantity_destroyed":6,"singleton":0},{"flag":32,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":33,"item_type_id":33450,"quantity_dropped":1,"singleton":0},{"flag":92,"item_type_id":26374,"quantity_destroyed":1,"singleton":0},{"flag":24,"item_type_id":2281,"quantity_destroyed":1,"singleton":0},{"flag":5,"item_type_id":24507,"quantity_destroyed":15366,"singleton":0},{"flag":30,"item_type_id":33450,"quantity_dropped":1,"singleton":0},{"flag":31,"item_type_id":24507,"quantity_destroyed":10,"singleton":0},{"flag":12,"item_type_id":1248,"quantity_destroyed":1,"singleton":0},{"flag":15,"item_type_id":8135,"quantity_destroyed":1,"singleton":0},{"flag":5,"item_type_id":28668,"quantity_dropped":17,"singleton":0},{"flag":87,"item_type_id":31890,"quantity_destroyed":2,"singleton":0},{"flag":87,"item_type_id":31890,"quantity_dropped":3,"singleton":0},{"flag":22,"item_type_id":2281,"quantity_dropped":1,"singleton":0},{"flag":87,"item_type_id":31894,"quantity_destroyed":3,"singleton":0},{"flag":87,"item_type_id":31894,"quantity_dropped":2,"singleton":0},{"flag":93,"item_type_id":4397,"quantity_destroyed":1,"singleton":0},{"flag":17,"item_type_id":2048,"quantity_dropped":1,"singleton":0},{"flag":21,"item_type_id":13953,"quantity_destroyed":1,"singleton":0},{"flag":19,"item_type_id":5443,"quantity_dropped":1,"singleton":0},{"flag":5,"item_type_id":17938,"quantity_dropped":1,"singleton":0},{"flag":31,"item_type_id":33450,"quantity_dropped":1,"singleton":0},{"flag":87,"item_type_id":2488,"quantity_destroyed":4,"singleton":0},{"flag":87,"item_type_id":2488,"quantity_dropped":2,"singleton":0},{"flag":23,"item_type_id":41218,"quantity_destroyed":1,"singleton":0},{"flag":13,"item_type_id":1248,"quantity_destroyed":1,"singleton":0}],"position":{"x":-106675567338.35883,"y":-53284704281.52799,"z":637580135899.0309},"ship_type_id":47466}},"zkb":{"locationID":40406890,"hash":"df5710fe59fd96e09cbaa381cd52906bc7f919a6","fittedValue":374428881.32,"droppedValue":43312011.33,"destroyedValue":378840295.1,"totalValue":422152306.43,"points":7,"npc":false,"solo":false,"awox":false,"labels":["cat:6","#:5+","pvp","loc:w-space"],"href":"https://esi.evetech.net/v1/killmails/110946162/df5710fe59fd96e09cbaa381cd52906bc7f919a6/"}}}'
+			// case dead
+			killboardResponse = '{"package":{"killID":110956448,"killmail":{"attackers":[{"alliance_id":99005866,"character_id":2120719397,"corporation_id":98733323,"damage_done":9863,"final_blow":true,"security_status":4.8,"ship_type_id":29990,"weapon_type_id":24488},{"corporation_id":1000124,"damage_done":5666,"final_blow":false,"security_status":0,"ship_type_id":10018}],"killmail_id":110956448,"killmail_time":"2023-08-12T21:00:22Z","solar_system_id":30003425,"victim":{"character_id":2121502096,"corporation_id":98578021,"damage_taken":15529,"items":[{"flag":28,"item_type_id":17482,"quantity_destroyed":1,"singleton":0},{"flag":13,"item_type_id":22542,"quantity_dropped":1,"singleton":0},{"flag":27,"item_type_id":17482,"quantity_dropped":1,"singleton":0},{"flag":87,"item_type_id":2486,"quantity_dropped":1,"singleton":0},{"flag":20,"item_type_id":6569,"quantity_destroyed":1,"singleton":0},{"flag":19,"item_type_id":35656,"quantity_dropped":1,"singleton":0},{"flag":134,"item_type_id":17452,"quantity_destroyed":2323,"singleton":0},{"flag":134,"item_type_id":1227,"quantity_dropped":8250,"singleton":0},{"flag":87,"item_type_id":10246,"quantity_destroyed":4,"singleton":0},{"flag":94,"item_type_id":31119,"quantity_destroyed":1,"singleton":0},{"flag":11,"item_type_id":22542,"quantity_dropped":1,"singleton":0},{"flag":12,"item_type_id":22542,"quantity_dropped":1,"singleton":0}],"position":{"x":910895285957.1665,"y":42905149874.17961,"z":2238955311219.2793},"ship_type_id":17478}},"zkb":{"locationID":40217140,"hash":"cf24975125661196a07bbe51a6e802d4a07f96cb","fittedValue":69283771.13,"droppedValue":8105890.65,"destroyedValue":65563861.74,"totalValue":10000,"points":3,"npc":false,"solo":true,"awox":false,"labels":["cat:6","solo","pvp","loc:lowsec"],"href":"https://esi.evetech.net/v1/killmails/110956448/cf24975125661196a07bbe51a6e802d4a07f96cb/"}}}'
 			redisqData = JSON.parse(killboardResponse);
 		}
 
@@ -71,7 +76,7 @@ async function processKillmail(debug = false) {
 				pushKillmailMsg(redisqData.package, 'lost', newbeeAttackerIDs);
 			// check attackers
 			else if (newbeeAttackerIDs.length)
-				pushKillmailMsg(redisqData.package, 'killed', newbeeAttackerIDs);
+				pushKillmailMsg(redisqData.package, 'kill', newbeeAttackerIDs);
 		}
 
 		if (debug)
@@ -114,11 +119,31 @@ async function pushKillmailMsg(package, type, newbeeAttackerIDs) {
 	//console.log(idMap);
 
 	//                                      true : false
-	let embedColor = (type == 'lost') ? 0xFF0000 : 0x00FFFF
+	const embedColor = (type == 'lost') ? 0xFF0000 : 0x00FFFF
 	//console.log(embedColor);
+
 	//                  Kill: Goem Funaila (Keres)
 	const embedTitle = 'Kill: '+idMap.get(package.killmail.victim.character_id)+' ('+idMap.get(package.killmail.victim.ship_type_id)+')';
 	//console.log(embedTitle);
+	// case lost) newbee **Goem Funaila** lost
+	//            flying in a **Praxis**
+	//            Final Blowed by
+	//            **Goem Funaila** flying in a **Praxis** with Caldari Navy Nova Heavy Assault Missile
+	//
+	// case kill) newbee killed:
+	//            **Goem Funaila** flying in a **Praxis**
+	//            Involved Newbees:
+	//            **Goem Funaila** flying in a **Praxis** with Caldari Navy Nova Heavy Assault Missile | **Goem Funaila** flying in a **Praxis** with Caldari Navy Nova Heavy Assault Missile
+	
+	//                                                                                                             true : false
+	const killmailfFieldName = (type == 'lost') ? 'newbee **'+idMap.get(package.killmail.victim.character_id)+'** lost' : 'newbee killed:';
+
+	const flyingInAShip = 'flying in a **'+idMap.get(package.killmail.victim.ship_type_id)+'**';
+	const killmailfFieldValue = (type == 'lost') ? flyingInAShip : '**'+idMap.get(package.killmail.victim.character_id)+'** '+flyingInAShip;
+	//                                                  ^   true : false
+
+	//                                                         true : false
+	const elementFieldHeader = (type == 'lost') ? 'Final Blowed by' : 'Involved Newbees:';
 
 	const killmailEmbed = new EmbedBuilder()
 		.setColor(embedColor)
@@ -127,7 +152,7 @@ async function pushKillmailMsg(package, type, newbeeAttackerIDs) {
 		.setDescription('**Location:** \n'+idMap.get(package.killmail.solar_system_id))
 		.setThumbnail('https://images.evetech.net/types/'+package.killmail.victim.ship_type_id+'/render?size=128')
 		.addFields(
-			{ name: 'newbee '+type+':', value: idMap.get(package.killmail.victim.character_id)+' flying in a **'+idMap.get(package.killmail.victim.ship_type_id)+'**' },
+			{ name: killmailfFieldName, value: killmailfFieldValue},
 		);
 	
 	var isFristElement = true;
@@ -135,14 +160,14 @@ async function pushKillmailMsg(package, type, newbeeAttackerIDs) {
 		var fieldName = '\u200b';
 
 		if (isFristElement) {
-			fieldName = 'contributers:';
+			fieldName = elementFieldHeader;
 			isFristElement = false;
 		}
 
 		killmailEmbed.addFields({ name: fieldName, value: '**'+idMap.get(element.character_id)+'** flying in a '+getShipAndWeaponString(idMap.get(element.ship_type_id), idMap.get(element.weapon_type_id)), inline: true })
 	}
 
-	killmailEmbed.addFields({ name: '\u200b', value: '**Total '+getPriceString(package.zkb.totalValue)+' ISK**, Droped '+getPriceString(package.zkb.droppedValue)+' ISK' });
+	killmailEmbed.addFields({ name: '\u200b', value: '**Total '+getPriceString(package.zkb.totalValue)+' ISK**, Droped '+getPriceString(package.zkb.droppedValue)+' ISK, Involved: **'+package.killmail.attackers.length+'**' });
 
 	client.channels.cache.get(process.env.DISCORD_CHANNEL_ID).send({
 		content: '뉴비 연관 킬메일 발생!',
@@ -160,9 +185,8 @@ function getPriceString(rawKillmailPrice) {
 }
 
 function getShipAndWeaponString(ship, weapon) {
-	var returnString = '';
+	const returnString = '**'+ship+'**';
 
-	returnString = returnString + ship;
 	if (ship == weapon)
 		return returnString;
 

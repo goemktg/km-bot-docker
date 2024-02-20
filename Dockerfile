e2d21a7d8446d30c5c93846669974297ed789f0f
goemktg <samktg52@gmail.com>
2024-02-20 19:07:43 +0000

    build: 빌드에 라이브러리가 누락된 점 수정, 빌드에서 개발용 라이브러리를 사용하지 않도록 수정

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:21-slim
 WORKDIR /usr/src/app
 COPY ./package.json ./
-RUN npm install
+RUN npm install --omit=dev
 COPY ./index.js ./
+COPY ./library/* ./library/
 CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const { Client, Events, GatewayIntentBits, EmbedBuilder, ActivityType } = require('discord.js');
-const cron = require("node-cron")
+const { loadEnvironmentVariables } = require('./library/functions.js');
+const cron = require('node-cron');
+const axios = require('axios');
 
-// Create a new client instance
+loadEnvironmentVariables();
+
+// client 인스턴스 생성
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// load from .env if exists
-try {
-	const dotenv = require('dotenv');
-	dotenv.config();
-} catch (ex) {
-	console.log('.env not found. using docker environment variable')
-}
-
-// Log in to Discord with your client's token
+// discord token을 사용해 client 로그인
 client.login(process.env.DISCORD_TOKEN);
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+// client가 준비되면 이 코드 실행 ( 한번만 실행됨 )
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 
+	// 봇 상태메시지 설정
 	client.user.setPresence({
 		activities: [{
 			type: ActivityType.Custom,
-			name: "custom", // name is exposed through the API but not shown in the client for ActivityType.Custom
-			state: "킬보드 감시 중",
-		}]
+			name: 'custom',
+			state: '킬보드 감시 중',
+		}],
 	});
 });
 
@@ -34,19 +30,20 @@ const redisqURL = 'https://redisq.zkillboard.com/listen.php?queueID=Goem_Funaila
 console.log('getting data from:' + redisqURL);
 
 let isJobRunning = false;
-cron.schedule("* * * * *", async function () {
+cron.schedule('* * * * *', async function() {
 	if (!isJobRunning) {
 		console.log('time to get data!');
 		processKillmail();
 	}
-	else
+	else {
 		console.log('time to get data. but job is already running.');
+	}
 });
 
 async function processKillmail() {
-	//console.log("running a task every 10 seconds");
 	isJobRunning = true;
-	// redisq zkillboard api 
+	// TODO: 요거 reamde로 옮기기
+	// redisq zkillboard api
 	// retives one killmail at one time
 	// job flow
 	// 1) 10초마다 2) 실행
@@ -57,8 +54,8 @@ async function processKillmail() {
 	let isKillmailExist = true;
 
 	while (isKillmailExist) {
-		var killboardResponse = await fetch(redisqURL, { method: "GET" });
-		var redisqData = await JSON.parse(await killboardResponse.text());
+		const redisqData = await axios.get(redisqURL)
+			.then(response => response.data);
 
 		if (redisqData.package == null) {
 			console.log('no killmail returned. skip killmail process.');
@@ -68,30 +65,32 @@ async function processKillmail() {
 
 			console.log('processed: ' + redisqData.package.killID);
 
-			// create newbee attacker id array
-			var newbeeAttackerIDs = new Array();
-			// create oldbee attacker id array
-			var oldbeeAttackerIDs = new Array();
-			for (let element of redisqData.package.killmail.attackers) {
+			const newbeeAttackerIDs = new Array();
+			const oldbeeAttackerIDs = new Array();
+			for (const element of redisqData.package.killmail.attackers) {
 
-				if (element.corporation_id == 98578021 || element.final_blow == true && redisqData.package.killmail.victim.corporation_id == 98578021) {
+				if (element.corporation_id == 98578021 || (element.final_blow == true && redisqData.package.killmail.victim.corporation_id == 98578021)) {
 					newbeeAttackerIDs.push(element);
 				}
 
-				if (element.alliance_id == 99010412 && redisqData.package.zkb.totalValue >= 100000000)
+				if (element.alliance_id == 99010412 && redisqData.package.zkb.totalValue >= 100000000) {
 					oldbeeAttackerIDs.push(element);
+				}
 			}
 
 			// check victim
-			if (redisqData.package.killmail.victim.corporation_id == 98578021)
+			if (redisqData.package.killmail.victim.corporation_id == 98578021) {
 				pushKillmailMsg(redisqData.package, 'lost', newbeeAttackerIDs);
+			}
 			// check attackers
-			else if (newbeeAttackerIDs.length)
+			else if (newbeeAttackerIDs.length) {
 				pushKillmailMsg(redisqData.package, 'kill', newbeeAttackerIDs);
+			}
 
 			// check alliance kill
-			if (oldbeeAttackerIDs.length)
+			if (oldbeeAttackerIDs.length) {
 				client.channels.cache.get(process.env.DISCORD_KM_POST_CHANNEL_ID).send('https://zkillboard.com/kill/' + redisqData.package.killID + '/');
+			}
 		}
 	}
 
@@ -100,12 +99,12 @@ async function processKillmail() {
 
 async function pushKillmailMsg(package, type, newbeeAttackerIDs) {
 	// make using id as array
-	var idMap = new Map();
+	const idMap = new Map();
 	idMap.set(package.killmail.victim.ship_type_id, null);
 	idMap.set(package.killmail.victim.character_id, null);
 	idMap.set(package.killmail.solar_system_id, null);
 
-	for (let element of newbeeAttackerIDs) {
+	for (const element of newbeeAttackerIDs) {
 
 		idMap.set(element.character_id, null);
 		idMap.set(element.ship_type_id, null);
@@ -115,25 +114,23 @@ async function pushKillmailMsg(package, type, newbeeAttackerIDs) {
 	const postData = JSON.stringify(Array.from(idMap.keys()));
 
 	// get name resolved
-	const EsiResponse = await fetch("https://esi.evetech.net/latest/universe/names/?datasource=tranquility", { method: "POST", headers: { 'User-Agent': 'Maintainer: Goem Funaila(IG) samktg52@gmail.com' }, body: postData })
-	const EsiData = await JSON.parse(await EsiResponse.text());
-
-	//console.log('Esi: ')
-	//console.log(EsiData);
+	const EsiResponse = await axios.post('https://esi.evetech.net/latest/universe/names/?datasource=tranquility', postData, {
+		headers: {
+			'User-Agent': 'Maintainer: Goem Funaila(IG) samktg52@gmail.com',
+		},
+	});
+	const EsiData = EsiResponse.data;
 
 	// apply esi data
-	for (let element of EsiData) {
+	for (const element of EsiData) {
 		idMap.set(element.id, element.name);
 	}
-	//console.log(idMap);
 
 	//                                      true : false
-	const embedColor = (type == 'lost') ? 0xFF0000 : 0x00FFFF
-	//console.log(embedColor);
+	const embedColor = (type == 'lost') ? 0xFF0000 : 0x00FFFF;
 
 	//                  Kill: Goem Funaila (Keres)
 	const embedTitle = 'Kill: ' + idMap.get(package.killmail.victim.character_id) + ' (' + idMap.get(package.killmail.victim.ship_type_id) + ')';
-	//console.log(embedTitle);
 	// case lost) newbee **Goem Funaila** lost
 	//            flying in a **Praxis**
 	//            Final Blowed by
@@ -164,16 +161,16 @@ async function pushKillmailMsg(package, type, newbeeAttackerIDs) {
 			{ name: killmailfFieldName, value: killmailfFieldValue },
 		);
 
-	var isFristElement = true;
-	for (let element of newbeeAttackerIDs) {
-		var fieldName = '\u200b';
+	let isFristElement = true;
+	for (const element of newbeeAttackerIDs) {
+		let fieldName = '\u200b';
 
 		if (isFristElement) {
 			fieldName = elementFieldHeader;
 			isFristElement = false;
 		}
 
-		killmailEmbed.addFields({ name: fieldName, value: '**' + idMap.get(element.character_id) + '** flying in a ' + getShipAndWeaponString(idMap.get(element.ship_type_id), idMap.get(element.weapon_type_id)), inline: true })
+		killmailEmbed.addFields({ name: fieldName, value: '**' + idMap.get(element.character_id) + '** flying in a ' + getShipAndWeaponString(idMap.get(element.ship_type_id), idMap.get(element.weapon_type_id)), inline: true });
 	}
 
 	killmailEmbed.addFields({ name: '\u200b', value: '**Total ' + getPriceString(package.zkb.totalValue) + ' ISK**, Droped ' + getPriceString(package.zkb.droppedValue) + ' ISK, Involved: **' + package.killmail.attackers.length + '**' });
@@ -184,22 +181,24 @@ async function pushKillmailMsg(package, type, newbeeAttackerIDs) {
 	});
 }
 
+
+// TODO: getPriceString 함수와 getFormatedNumString 함수를 통합할 수 있을 것 같음
 function getPriceString(rawKillmailPrice) {
-	if (rawKillmailPrice >= 1000000000) // 1b 이상
-		return getFormatedNumString(rawKillmailPrice / 1000000000) + 'B';
-	else if (rawKillmailPrice >= 1000000) // 1m 이상
-		return getFormatedNumString(rawKillmailPrice / 1000000) + 'M';
-	else
-		return getFormatedNumString(rawKillmailPrice);
-}
+	let returnString = '';
 
-function getShipAndWeaponString(ship, weapon) {
-	const returnString = '**' + ship + '**';
+	if (rawKillmailPrice >= 1000000000) {
+		// 1b 이상
+		returnString = getFormatedNumString(rawKillmailPrice / 1000000000) + 'B';
+	}
+	else if (rawKillmailPrice >= 1000000) {
+		// 1m 이상
+		returnString = getFormatedNumString(rawKillmailPrice / 1000000) + 'M';
+	}
+	else {
+		returnString = getFormatedNumString(rawKillmailPrice);
+	}
 
-	if (ship == weapon)
-		return returnString;
-
-	return returnString + ' with ' + weapon;
+	return returnString;
 }
 
 function getFormatedNumString(float) {
@@ -207,4 +206,14 @@ function getFormatedNumString(float) {
 		minimumFractionDigits: 2,
 		maximumFractionDigits: 2,
 	});
+}
+
+function getShipAndWeaponString(ship, weapon) {
+	const returnString = '**' + ship + '**';
+
+	if (ship == weapon) {
+		return returnString;
+	}
+
+	return returnString + ' with ' + weapon;
 }
